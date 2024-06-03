01f9de6cd3cba1244232f60b1cb88c2912f77a1c
iti-rina <itigecheva.irina@gmail.com>
2024-06-04 01:20:55 +0700

    Default code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const usersList = [
+  { name: "Julie Crawford" },
+  { name: "Billy Williams" },
+  { name: "Melvin Smith" },
+  { name: "AdolElaine Myersf" },
+  { name: "Dennis Lynch" },
+  { name: "Grace Walker" },
+  { name: "Catherine Martinez" },
+];
+
+function App() {
+  const [activeElement, setActiveElement] = useState(0);
+
+  return (
+    <div>
+      <ul>
+        {(usersList || [{}]).map((user, index) => (
+          <ListItem
+            key={index}
+            user={{ ...user, active: activeElement === index }}
+            onSetActiveClick={() => {
+              setActiveElement(index);
+            }}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const ListItem = ({ user, onSetActiveClick }) => {
+  return (
+    <li>
+      {user.name} {user.active && "(active)"}
+      <Button text="set active" onClick={onSetActiveClick} />
+    </li>
+  );
+};
+
+const Button = ({ text, onClick }) => {
+  console.log("button rerender");
+
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
