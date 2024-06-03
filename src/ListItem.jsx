9022c6bd7fe6b8eb0c32d2285fbdc69f022660d5
iti-rina <itigecheva.irina@gmail.com>
2024-06-04 02:04:38 +0700

    Add function to mark occurencies

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,7 +1,11 @@
 import { Button } from "./Button";
+import { highlight } from "./utils";
 
 export const ListItem = ({ user, isActive, handleActivate }) => {
   let name = user.name;
+  if (user?.highlight) {
+    name = highlight(user.name, user.highlight, user.hightlightLen);
+  }
 
   return (
     <li>
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export function findAllOccurrences(list, query) {
   for (let i = 0; i < list.length; i++) {
     let lowerCaseName = list[i].name.toLowerCase();
     let indices = [];
-    
+
     let queryIndexInStr = lowerCaseName.indexOf(lowerCaseQuery);
     
     while (queryIndexInStr > -1) {
@@ -18,4 +18,16 @@ export function findAllOccurrences(list, query) {
     }
   }
   return filteredList;
+}
+
+export function highlight(str, idices, highlightLen) {
+  let result = [];
+  let start = 0;
+  for (let index of idices) {
+    result.push(str.substring(start, index));
+    result.push(<mark>{str.substring(index, index + highlightLen)}</mark>);
+    start = index + highlightLen;
+  }
+  result.push(str.substring(start));
+  return result;
 }
