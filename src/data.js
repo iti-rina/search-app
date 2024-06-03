48c6e9813f20ba36906bf7d5a7dad5fcd9277060
iti-rina <itigecheva.irina@gmail.com>
2024-06-04 01:32:42 +0700

    Modify users' data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-const usersList = [
-  { name: "Julie Crawford" },
-  { name: "Billy Williams" },
-  { name: "Melvin Smith" },
-  { name: "AdolElaine Myersf" },
-  { name: "Dennis Lynch" },
-  { name: "Grace Walker" },
-  { name: "Catherine Martinez" },
+export const USER_LIST = [
+  { name: "Julie Crawford", id: '22922592-04af-4ecb-a556-e4ce316655da' },
+  { name: "Billy Williams", id: '98e36fda-bf25-4cbe-a0ef-3460809d622e' },
+  { name: "Melvin Smith", id: '48028440-15c1-4b91-a511-c56d6c949a4f' },
+  { name: "AdolElaine Myersf", id: '199698ae-4285-4a31-8e29-1a4455ecdaab' },
+  { name: "Dennis Lynch", id: '1fbb65d6-4d81-4a6b-9433-f87b1059ffc5' },
+  { name: "Grace Walker", id: '1ce7fecf-591a-4e20-b429-ed26ecad4101' },
+  { name: "Catherine Martinez", id: '67ca2870-1516-485d-84f6-2390772c7da6' },
 ];
